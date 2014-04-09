98cd7e651a16c3b1f2f48033571059b1ebd17f1d
Ryan Sukale <ryansukale@gmail.com>
2014-04-09 10:35:57 -0400

    Upgdraded bootstrap, moved min file to js/lib folder

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
-//Uncomment lines to include the libraries you need
+//Uncomment lines to include libraries as needed
 
 require.config({
     paths: {
         //localstorage: "lib/backbone.localStorage",
         modernizr: "lib/modernizr-2.7.1.min",
+        bootstrap: "lib/bootstrap-3.1.1.min",
         jquery: "lib/jquery-1.9.1.min",
         underscore: "lib/underscore.min",
         backbone: "lib/backbone.min"
