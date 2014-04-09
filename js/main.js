8ee2c90db407b2c9dc6d4e950919baffdfa86bb6
Ryan Sukale <ryansukale@gmail.com>
2014-04-09 10:41:28 -0400

    Added json2 and jqueryUI modules

--- js/main.js
+++ js/main.js
@@ -5,6 +5,8 @@ require.config({
         //localstorage: "lib/backbone.localStorage",
         modernizr: "lib/modernizr-2.7.1.min",
         bootstrap: "lib/bootstrap-3.1.1.min",
+        jqueryUI: "lib/jquery-ui-1.10.0.custom.min",
+        json2: "lib/json2",
         jquery: "lib/jquery-1.9.1.min",
         underscore: "lib/underscore.min",
         backbone: "lib/backbone.min"
