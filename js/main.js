f7f56069fa6326ab3d7de9140e36b3bd8ccc8a37
Ryan Sukale <ryansukale@gmail.com>
2014-04-09 10:53:49 -0400

    Added a shim for backbonejs based upon these links
    http://requirejs.org/docs/api.html#config-shim
    http://gregfranko.com/blog/require-dot-js-2-dot-0-shim-configuration/

--- js/main.js
+++ js/main.js
@@ -9,6 +9,17 @@ require.config({
         json2: "lib/json2",
         jquery: "lib/jquery-1.9.1.min",
         underscore: "lib/underscore.min",
-        backbone: "lib/backbone.min"
+        backbone: "lib/backbone.min",
+        shim: {
+        	'backbone': {
+            //These script dependencies should be loaded before loading backbone.js
+            deps: ['underscore', 'jquery'],
+            //Once loaded, use the global 'Backbone' as the module value.
+            exports: 'Backbone'
+	        },
+	        'underscore': {
+	            exports: '_'
+	        }
+        }
     }
 });
