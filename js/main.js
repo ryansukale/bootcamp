85bf87392a5534eab96354db8c107dd89415c9a5
Ryan Sukale <ryansukale@gmail.com>
2014-04-09 10:16:13 -0400

    Added backbonelocalstorage, initialized basic requirejs

---
+++ js/main.js
@@ -0,0 +1,10 @@
+//Uncomment lines to include the libraries you need
+
+require.config({
+    paths: {
+        //localstorage: "lib/backbone.localStorage",
+        jquery: "lib/jquery-1.9.1.min",
+        underscore: "lib/underscore",
+        backbone: "lib/backbone"
+    }
+});
