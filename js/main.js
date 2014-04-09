211a7a117e05db264ab4c0fe36c96e5dad8fe267
Ryan Sukale <ryansukale@gmail.com>
2014-04-09 10:29:32 -0400

    Updated modernizr, moved to lib directory, including min scripts by default

--- js/main.js
+++ js/main.js
@@ -3,8 +3,9 @@
 require.config({
     paths: {
         //localstorage: "lib/backbone.localStorage",
+        modernizr: "lib/modernizr-2.7.1.min",
         jquery: "lib/jquery-1.9.1.min",
-        underscore: "lib/underscore",
-        backbone: "lib/backbone"
+        underscore: "lib/underscore.min",
+        backbone: "lib/backbone.min"
     }
 });
