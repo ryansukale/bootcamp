b2e069460d0b5c635ba2a7c663372a55cc27dc96
Ryan Sukale <ryansukale@gmail.com>
2014-04-09 23:04:43 -0400

    Fixed shim misconfigured in require params

--- js/main.js
+++ js/main.js
@@ -1,25 +1,27 @@
 //Uncomment lines to include libraries as needed
 
-require.config({
+var params = {
     paths: {
         //localstorage: "lib/backbone.localStorage",
+        json2: "lib/json2",
         modernizr: "lib/modernizr-2.7.1.min",
         bootstrap: "lib/bootstrap-3.1.1.min",
-        jqueryUI: "lib/jquery-ui-1.10.0.custom.min",
-        json2: "lib/json2",
         jquery: "lib/jquery-1.9.1.min",
+        jqueryUI: "lib/jquery-ui-1.10.0.custom.min",
         underscore: "lib/underscore.min",
-        backbone: "lib/backbone.min",
-        shim: {
-        	'backbone': {
-            //These script dependencies should be loaded before loading backbone.js
-            deps: ['underscore', 'jquery'],
-            //Once loaded, use the global 'Backbone' as the module value.
-            exports: 'Backbone'
-	        },
-	        'underscore': {
-	            exports: '_'
-	        }
+        backbone: "lib/backbone.min"
+    },
+    shim: {
+        'backbone': {
+          //These script dependencies should be loaded before loading backbone.js
+          deps: ['underscore', 'jquery'],
+          //Once loaded, use the global 'Backbone' as the module value.
+          exports: 'Backbone'
+        },
+        'underscore': {
+            exports: '_'
         }
     }
-});
+};
+
+require.config(params);
