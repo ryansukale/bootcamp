30fbc89eaa5c6c7693042f459abacc7cb911e782
Ryan Sukale <ryansukale@gmail.com>
2014-04-23 11:59:14 -0400

    Added Starter file for a Marionette app

--- js/app/app.js
+++ js/app/app.js
@@ -11,17 +11,35 @@ require(
 				var App = new Backbone.Marionette.Application();
 				App.on('initialize:before', function(options) {
 					// Do something .e.g. you can add more data to your options
+					console.log('App initialized:before');
 				});
 
 				App.on('initialize:after', function(options) {
 					// Do something else
+					console.log('App initialized:after');
 				});
 
 				App.on('start', function(options) {
 					if (Backbone.history){
 				    Backbone.history.start(); // Ref:https://github.com/marionettejs/backbone.marionette/blob/master/docs/marionette.application.md
 				  }
+				  console.log('App started');
 				});
+
+				// https://github.com/davidsulc/structuring-backbone-with-requirejs-and-marionette/blob/master/assets/js/app.js
+				App.startSubApp = function(appName, args){
+			    var currentApp = appName ? App.module(appName) : null;
+			    if (App.currentApp === currentApp){ return; }
+
+			    if (App.currentApp){
+			      App.currentApp.stop();
+			    }
+
+			    App.currentApp = currentApp;
+			    if(currentApp){
+			      currentApp.start(args);
+			    }
+			  };
 
 				// Start the app once the DOM is ready
 				$(function(){
